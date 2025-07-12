4229f5cd9654a7a079b69910c3934428215c6a7c
AnshMehta24 <ansh.mehta.y24@gmail.com>
2025-07-12 12:51:42 +0530

    Fixed App.tsx

---
+++ client/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter } from "react-router";
+import Router from "./routes";
+import { ThemeProvider } from "./context/theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { Toaster } from "./components/ui/sonner";
+
+const queryClient = new QueryClient({
+  defaultOptions: { queries: { retry: false } },
+});
+
+const App = () => (
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        {/* <Toaster richColors /> */}
+        <Router />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  </BrowserRouter>
+);
+
+export default App;
--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,10 +1,23 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
+import QuestionsPage from "./pages/QuestionsPage";
+import AskQuestionPage from "./pages/AskQuestionPage";
+import QuestionDetailPage from "./pages/QuestionDetailPage";
+import TagsPage from "./pages/TagsPage";
+import NotFound from "./pages/NotFound";
 
 const routes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/" element={<QuestionsPage />} />
+      <Route path="/ask" element={<AskQuestionPage />} />
+      <Route path="/questions/:id" element={<QuestionDetailPage />} />
+      <Route path="/tags" element={<TagsPage />} />
+      <Route path="/tags/:tag" element={<QuestionsPage />} />
+      {/* <Route path="/users/:username" element={<UserProfilePage />} /> */}
+      {/* <Route path="/profile" element={<UserProfilePage />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
